fe8da54b7c744c367b98dbde53fd420c700f2f66
Vladislav Magdenko <vladd0193@gmail.com>
2017-12-20 23:55:14 +0200

    Update my-script.js

--- js/my-script.js
+++ js/my-script.js
@@ -174,5 +174,12 @@ var x = setInterval(function() {
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
-
+   // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = 0;
+    document.getElementById("hours").innerHTML = 0;
+    document.getElementById("minutes").innerHTML = 0;
+    document.getElementById("seconds").innerHTML = 0;
+  }
 }, 1000);
